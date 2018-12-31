388a91dbc49d5b171e43d576762acaf980f3e409
vladimirkravchenko <vovchik33@gmail.com>
2018-12-31 19:47:31 +0200

    Demo-0015: Implemented adding new task

--- src/demos/demo_0015/components/TodoApp/TodoForm/todoform.component.tsx
+++ src/demos/demo_0015/components/TodoApp/TodoForm/todoform.component.tsx
@@ -3,13 +3,41 @@ import {ITodosStoreProps} from "../../../stores/todos.store";
 
 import "./todoform.component.scss";
 
-export default class TodoFormComponent extends React.Component<ITodosStoreProps> {
+interface IState {
+  newTaskTitle: string
+}
+export default class TodoFormComponent extends React.Component<ITodosStoreProps, IState> {
+  constructor(props: Readonly<ITodosStoreProps>) {
+    super(props);
+    this.state = {
+      newTaskTitle: ""
+    }
+  }
+
   public render(): React.ReactNode {
     return (
       <div className="todoform-container">
         <span className="todoform-label">Input task title:</span>
-        <input className="todoform-input" type="text" placeholder="task title"/>
+        <input className="todoform-input" type="text" placeholder="task title"
+               value={this.state.newTaskTitle}
+               onKeyPress={this.keypressTaskTitleHandler}
+               onChange={this.changeTaskTitleHandler}/>
       </div>
     );
+  }
+
+  private keypressTaskTitleHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key==="Enter") {
+      this.props.store.addTask(this.state.newTaskTitle);
+      this.setState({
+        newTaskTitle: ""
+      })
+    }
+  };
+
+  private changeTaskTitleHandler = (event:React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      newTaskTitle: event.currentTarget.value
+    })
   }
 }
--- src/demos/demo_0015/stores/todos.store.ts
+++ src/demos/demo_0015/stores/todos.store.ts
@@ -12,10 +12,16 @@ export default class TodosStore {
     new TodoStore({id: 2, title: "Todo 2", state: TodoStateType.OPEN}),
     new TodoStore({id: 3, title: "Todo 3", state: TodoStateType.IN_PROGRESS}),
     new TodoStore({id: 4, title: "Todo 4", state: TodoStateType.DONE}),
-    new TodoStore({id: 4, title: "Todo 4", state: TodoStateType.CLOSED}),
-    new TodoStore({id: 4, title: "Todo 4", state: TodoStateType.CLOSED}),
-    new TodoStore({id: 4, title: "Todo 4", state: TodoStateType.CLOSED}),
+    new TodoStore({id: 5, title: "Todo 4", state: TodoStateType.CLOSED}),
+    new TodoStore({id: 6, title: "Todo 4", state: TodoStateType.CLOSED}),
+    new TodoStore({id: 7, title: "Todo 4", state: TodoStateType.CLOSED}),
   ];
   @observable public filterSearchString: string = "";
   @observable public filterState: number = TodoStateType.NONE;
+
+  public addTask = (newTaskTitle: string) => {
+    setTimeout(() => {
+      this.todos.push({id: 1, title: newTaskTitle, state: TodoStateType.OPEN})
+    }, 1000);
+  }
 }
