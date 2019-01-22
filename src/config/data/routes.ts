b5a4354e0724d69d3bac1533ba0b8f7d13d21477
volodymyrkr <volodymyr.kravchenko.2000@gmail.com>
2019-01-22 23:55:03 +0200

    Demo-0017: Initial commit

--- src/config/data/routes.ts
+++ src/config/data/routes.ts
@@ -12,6 +12,7 @@ import Demo0012 from "../../demos/demo_0012";
 import Demo0013 from "../../demos/demo_0013";
 import Demo0014 from "../../demos/demo_0014";
 import Demo0015 from "../../demos/demo_0015";
+import {Demo0017} from "../../demos/demo_0017";
 
 export interface IDemoProps {
   name: string,
@@ -35,6 +36,7 @@ export const demos:ReadonlyArray<IDemoProps> = [
   {name: "demo-0013", link: "/demo_0013", title: "Demo", description: "Punk API demo application with using mobx and fetch"},
   {name: "demo-0014", link: "/demo_0014", title: "Demo", description: "MobX Contacs demo is added"},
   {name: "demo-0015", link: "/demo_0015", title: "Demo", description: "Inversify demo"},
+  {name: "demo-0017", link: "/demo_0017", title: "Demo", description: "Mobx demo"},
 ];
 
 export const RouteComponentMapper = {
@@ -52,4 +54,5 @@ export const RouteComponentMapper = {
   "demo-0013": Demo0013,
   "demo-0014": Demo0014,
   "demo-0015": Demo0015,
+  "demo-0017": Demo0017,
 };
---
+++ src/demos/demo_0017/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const Demo0017: React.FC = () => {
+    return (
+      <div>Demo0017 is here</div>
+    )
+  };
