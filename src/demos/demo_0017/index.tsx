15f223def635fcf03944295d4cfc318b0dadf089
volodymyrkr <volodymyr.kravchenko.2000@gmail.com>
2019-01-24 23:35:14 +0200

    Demo_0017: Simple example

--- src/demos/demo_0017/index.tsx
+++ src/demos/demo_0017/index.tsx
@@ -1,9 +1,38 @@
 import * as React from "react";
+interface IDemoProps { someData: string }
 
-export class Demo0017 extends React.Component {
+interface IDemoState { someData: string }
+
+export class Demo0017 extends React.Component<IDemoProps, IDemoState> {
+  constructor(props: IDemoProps) {
+    super(props);
+    this.state = {
+      someData: ""
+    }
+  }
+  public execute(data: string) {
+    this.setState({
+      someData: data
+    });
+  }
+
+  public componentDidUpdate(prevProps: Readonly<IDemoProps>, prevState: Readonly<IDemoState>, snapshot?: any): void {
+    setTimeout(() => {
+      this.setState(({someData}) => (
+        {
+          someData:""
+        }
+      ))
+    }, 5000);
+  }
+
   public render() {
     return (
-      <div>Demo0017 is here</div>
+      <React.Fragment>
+        {
+          !!this.state && !!this.state.someData && <div>Demo0017 is here {this.state.someData}</div>
+        }
+      </React.Fragment>
     )
   }
 }
